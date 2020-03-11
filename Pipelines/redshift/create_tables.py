a0f9432c5c1e2e06e100b0a29e75d8ef8d2bb2d2
mddarr <mddarr@gmail.com>
2020-03-11 14:13:00 -0700

    Start to add some scrapers

--- Pipelines/redshift/create_tables.py
+++ Pipelines/redshift/create_tables.py
@@ -1,3 +1,6 @@
 import configparser
 import psycopg2
 
+# Create the tables for the star schema
+
+
---
+++ Pipelines/snotel/webscraping.py
@@ -0,0 +1,47 @@
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+
+def parse_wta_trip_reports(url):
+    wtaClient = uReq(url)
+    wta_html = wtaClient.read()
+    wtaClient.close()
+    wtasoup = soup(wta_html,"html.parser")
+    return wtasoup
+
+def parse_book_reviews(url):
+    bookClient = uReq(url)
+    book_html = bookClient.read()
+    bookClient.close()
+    book_soup = soup(book_html,"html.parser")
+    reviewsTable = book_soup.find('div', attrs = {'id':'bookReviews'})
+    reviews = []
+    span_array = []
+    for review in reviewsTable.find_all('div', {"class": "friendReviews elementListBrown"}):
+        spans = review.find_all('span')
+        span_array.append(spans)
+        spans.pop()
+        spans.pop()
+        spans.pop()
+        spans.pop()
+        likes = int(spans.pop().text.split(' ')[0])
+        reviewtext = spans.pop().text
+
+        spans.reverse()
+        username = spans.pop().text
+        # Username will look like '\nKate\n',
+        username = username[1:-1]
+        reviews.append((username, reviewtext, likes) )
+    return book_soup
+
+book_url = 'https://www.goodreads.com/book/show/249.Tropic_of_Cancer'
+soup = parse_book_reviews(book_url)
+
+# spans = r1.find_all('span')
+# spans.pop()
+# spans.pop()
+# spans.pop()
+# # spans.pop()
+# likes  = int(spans.pop().text.split(' ')[0])
+# review1 = spans.pop()
+# reviewText = review1.text
+
